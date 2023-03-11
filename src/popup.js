8733820a142169d0f8efbe19ad49bc5c60b6d7f5
Yuguang Wang <yuwang@redhat.com>
2023-03-11 12:46:10 +0800

    Make the popup more compact

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,12 @@
-// Get the "Apply" button and add a click event listener
-const applyColorButton = document.getElementById("apply-color-button");
-applyColorButton.addEventListener("click", function () {
-  // Get the selected color
-  const selectedColor = document.querySelector('input[name="color"]:checked').value;
-  // Send a message to the content script to change the background color
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "change-color", color: selectedColor });
+// Get all the color radio buttons and add a change event listener to each
+const colorRadios = document.querySelectorAll('input[name="color"]');
+colorRadios.forEach(function(radio) {
+  radio.addEventListener("change", function() {
+    // Get the selected color
+    const selectedColor = document.querySelector('input[name="color"]:checked').value;
+    // Send a message to the content script to change the background color
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "change-color", color: selectedColor });
+    });
   });
 });
